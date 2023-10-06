867a8cc31d009500a3f9b1ffd1b9dea71ecf6ad5
Revanthsatyam <satyamrevanth@gmail.com>
2023-10-06 12:25:08 +0530

    Terraform

--- 05-data-sources/main.tf
+++ 05-data-sources/main.tf
@@ -1,9 +1,9 @@
-data "aws_ami" "ami" {
-  most_recent      = true
+data "aws_amis" "ami" {
+  //most_recent      = true
   name_regex       = "Centos-*"
   owners           = ["973714476881"]
 }
 
 output "ami" {
-  value = data.aws_ami.ami
+  value = data.aws_amis.ami
 }
