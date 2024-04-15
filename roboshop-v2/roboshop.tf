d77debad7b2934f9ed1d9ac10b1fbd1948156267
Revanthsatyam <satyamrevanth@gmail.com>
2024-04-15 11:03:53 +0530

    Terraform Roboshop V2

---
+++ roboshop-v2/roboshop.tf
@@ -0,0 +1,52 @@
+variable "components" {
+  default = {
+    frontend  = { name = "frontend-dev" }
+    mongodb   = { name = "mongodb-dev" }
+    catalogue = { name = "catalogue-dev" }
+    redis     = { name = "redis-dev" }
+    user      = { name = "user-dev" }
+    cart      = { name = "cart-dev"}
+    mysql     = { name = "name-dev" }
+    shipping  = { name = "shipping-dev" }
+    rabbitmq  = { name = "rabbitmq-dev"}
+    payment   = { name = "payment-dev" }
+    shipping  = { name = "shipping-dev" }
+  }
+}
+
+variable "ami" {
+  default = "ami-0f3c7d07486cad139"
+}
+
+variable "instance_type" {
+  default = "t3.small"
+}
+
+variable "vpc_security_group_ids" {
+  default = [ "sg-0f15c7e71393537f6" ]
+}
+
+variable "zone_id" {
+  default = "Z09651852G8MXYMFFQDTV"
+}
+
+
+resource "aws_instance" "instance" {
+  for_each               = var.components
+  ami                    = var.ami
+  instance_type          = var.instance_type
+  vpc_security_group_ids = var.vpc_security_group_ids
+
+  tags = {
+    Name = lookup(each.value, "name", null)
+  }
+}
+
+resource "aws_route53_record" "www" {
+  for_each = var.components
+  zone_id  = var.zone_id
+  name     = "${each.key}.rdevops74.online"
+  type     = "A"
+  ttl      = 30
+  records  = [ lookup(aws_instance.instance[each.key], "name", null) ]
+}
